b8e16bdca4ff953256e9e1ad1bd6448f68279ada
Graham Bygrave <graham@bygrave.org>
2022-10-13 13:57:54 +0100

    Initial project add.

--- src/main/java/guru/sfg/beer/msscconfigserver/MsscConfigServerApplication.java
+++ src/main/java/guru/sfg/beer/msscconfigserver/MsscConfigServerApplication.java
@@ -2,7 +2,9 @@ package guru.sfg.beer.msscconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class MsscConfigServerApplication {
 
