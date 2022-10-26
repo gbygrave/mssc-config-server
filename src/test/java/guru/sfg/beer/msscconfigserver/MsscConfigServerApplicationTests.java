70a427098bba89d4b09214d85c4a50e58588ec72
Graham Bygrave <graham@bygrave.org>
2022-10-26 11:32:57 +0100

    Migrate to 2.3.0-RELEASE and add docker support. Closes #2.

--- src/test/java/guru/sfg/beer/msscconfigserver/MsscConfigServerApplicationTests.java
+++ src/test/java/guru/sfg/beer/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -2,8 +2,10 @@ package guru.sfg.beer.msscconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsscConfigServerApplicationTests {
 
     @Test
